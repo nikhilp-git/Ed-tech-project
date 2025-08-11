54a403ede9cc64c398b12b132aaec04adb101473
nikhilp-git <nikhilpapola2004@gmail.com>
2025-08-12 01:42:21 +0530

    Add files via upload

---
+++ config-overrides.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack');
+require("dotenv").config()
+
+module.exports = function override(config) {
+  config.resolve.fallback = {
+    "path": require.resolve("path-browserify"),
+    "os": require.resolve("os-browserify/browser"),
+    "crypto": require.resolve("crypto-browserify"),
+    "stream": require.resolve("stream-browserify"),
+    "http": require.resolve("stream-http"),
+    "https": require.resolve("https-browserify"),
+    "zlib": require.resolve("browserify-zlib"),
+    "url": require.resolve("url"),
+    "util": require.resolve("util"),
+  };
+
+  config.plugins.push(
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer'],
+    })
+  );
+
+  return config;
+};
